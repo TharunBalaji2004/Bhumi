8be4ecf301f814ebc963b49fd431096feac3a844
Tharun R <rtharun222@gmail.com>
2024-05-14 22:42:17 +0530

    Added Contact Us and Campaigns Page

--- src/Pages/Campaigns.jsx
+++ src/Pages/Campaigns.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Campcard from "@/components/ui/campcard";
+import Contact from "@/components/ui/contact";
 
 const Campaigns = () => {
   return (
-    <div>Campaigns</div>
-  )
-}
+    <>
+      <div className="flex flex-col justify-center bg-yellow-100">
+        <p className="flex justify-center pt-[30px] pb-[50px] text-4xl">
+          Our Campaigns
+        </p>
+        <div className="flex flex-row justify-around">
+          <Campcard></Campcard>
+          <Campcard></Campcard>
+          <Campcard></Campcard>
+        </div>
+      </div>
+      <Contact></Contact>
+    </>
+  );
+};
 
-export default Campaigns
+export default Campaigns;
