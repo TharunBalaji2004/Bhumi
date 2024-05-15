549100b39b4a50346b1889d54d3eef8859d1dfc6
Sriram <sriramsivakumar23@gmail.com>
2024-05-15 08:46:20 +0530

    navbar

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className=" h-14 w-full flex border-b-gray-200 border-b">
-      <div className=" w-1/5  flex justify-center items-center">
-        BHUMI
+    <div className="h-14 w-full flex border-b-gray-200 border-b">
+      <div className="w-1/5 flex justify-center items-center">
+        <h2 >BHUMI</h2>
       </div>
       <div className="flex flex-row justify-around items-center w-2/3 border-l-gray-200 border-l border-r border-r-gray-200">
-        <h1>Home</h1>
-        <h1>About us</h1>
-        <h1>Campaigns</h1>
-        <h1>Blogs</h1>
-        <h1>Volunteer</h1>
+        <Link to="/">Home</Link>
+        <Link to="/aboutus">About Us</Link>
+        <Link to="/campaigns">Campaigns</Link>
+        <Link to="/blogs">Blogs</Link>
+        <Link to="/volunteer">Volunteer</Link>
       </div>
-      <div className="w-1/5  flex justify-center items-center">
-        <h2>Donate Now</h2>
+      <div className="w-1/5 flex justify-center items-center">
+        <Link to="/donate">Donate Now</Link>
       </div>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
